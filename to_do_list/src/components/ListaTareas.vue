2c4d1f4212b5495cd7b9c9b9ed35adc0a428206f
KarlaBC <karlatataianabc@gmail.com>
2024-10-06 16:41:10 -0600

    Agregar la Funcionalidad de Marcar Tareas como Completadas

--- to_do_list/src/components/ListaTareas.vue
+++ to_do_list/src/components/ListaTareas.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="lista-tareas">
         <ul>
-            <li v-for="(tarea, index) in tareas" :key="index">
-                {{ tarea }}
+            <li v-for="(tarea, index) in tareasFiltradas" :key="index" :class="{ completada: tarea.completada }">
+                <span @click="marcarCompletada(index)">
+                    {{ tarea.titulo }}
+                </span>
                 <button @click="eliminarTarea(index)">Eliminar</button>
             </li>
         </ul>
@@ -11,10 +13,24 @@
 
 <script>
     export default {
-        props: ['tareas'],
+        props: ['tareas', 'filtro'],
+        computed: {
+            tareasFiltradas() {
+                if (this.filtro === 'completadas') {
+                    return this.tareas.filter(tarea => tarea.completada);
+                } else if (this.filtro === 'pendientes') {
+                    return this.tareas.filter(tarea => !tarea.completada);
+                } else {
+                    return this.tareas;
+                }
+            }
+        },
         methods: {
             eliminarTarea(index) {
-                this.$emit('tarea-eliminada', index);
+                this.$emit('eliminar-tarea', index);
+            },
+            marcarCompletada(index) {
+                this.$emit('marcar-completada', index);
             }
         }
     };
